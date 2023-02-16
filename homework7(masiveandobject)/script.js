7b1b25fe154e873675b48040984011bae6e7584b
Your Name <you@example.com>
2023-02-16 17:46:51 -0500

    a13

--- homework7(masiveandobject)/script.js
+++ homework7(masiveandobject)/script.js
@@ -1,10 +1,13 @@
 let pozy = { ingridients:['ground meat','onion', 'dough','eggs',]
 };
-
-
 //alert(pozy.ingridients);
 
 pozy.ingridients.pop();
 
 alert(pozy.ingridients);
 
+alert(pozy.ingridients[0]);
+
+pozy.ingridients.inshift('eggs');
+
+alert(pozy.ingridients[0]);
