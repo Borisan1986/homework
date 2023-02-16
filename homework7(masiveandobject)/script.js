273013ada78ad75d98be95735d5f316194e7e793
Your Name <you@example.com>
2023-02-16 16:44:19 -0500

    а15

--- homework7(masiveandobject)/script.js
+++ homework7(masiveandobject)/script.js
@@ -1,14 +1,13 @@
-let user = {
-    name: 'Dave',
-    age: 30,
-            'loves js':true
-  };
-  
-  alert(user["ioves js"]);
+let pozy = { ingridients:'ground meat','onion', 'dough','eggs'
+};
 
-  let favoriteMovieKey = 'Favorite moovie';
 
-  user[favoriteMovieKey] = 'Shrek 2';
 
-  alert(user[favoriteMovieKey]);
-  alert(user['favorite moovie']);
+
+
+
+
+
+
+
+alert([pozy.ingridients]);
