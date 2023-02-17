8eb5fd0cd13205fcbcbd22b84f6b9e6d4423f331
Your Name <you@example.com>
2023-02-17 11:38:15 -0500

    a15

--- homework7(masiveandobject)/script.js
+++ homework7(masiveandobject)/script.js
@@ -13,3 +13,12 @@ pozy.ingridients.unshift(`eggs`);
 alert(pozy.ingridients);
 pozy.ingridients.splice(1,1);
 alert(pozy.ingridients);
+
+let Prise = { 
+    ground meat:5,
+        onion:1,
+        dough:2,
+        eggs:2,
+    totalPrise: 10
+}
+        alert(Prise.totalPrise);
