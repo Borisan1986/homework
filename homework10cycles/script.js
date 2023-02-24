bcca17011f429e52797981980e6cb03b024a237f
Your Name <you@example.com>
2023-02-24 15:12:25 -0500

    й2

---
+++ homework10cycles/script.js
@@ -0,0 +1,26 @@
+for (let i = 1; i <= 10; i++){ 
+
+    if (i % 2 == 0) 
+    
+    { alert( i ); } 
+
+} 
+
+
+let i = 1;
+
+while( i > 0 ) { 
+	alert(i); 
+	if (i === 5) {
+		break;  
+	}
+
+	i++;
+}
+
+
+
+
+
+
+    
