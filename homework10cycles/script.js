af5890a96119a2f2be1a9619dabfb290c417a2bb
Your Name <you@example.com>
2023-03-02 21:15:09 -0500

    й1

--- homework10cycles/script.js
+++ homework10cycles/script.js
@@ -18,31 +18,80 @@ while( i > 0 ) {
 	i++;
 }
 
-let dish = [
+//Создайте массив блюд, с названием, ингредиентами, ценой приготовления и ценой реализации. 
+//⭐Не задавайте цену приготовления сразу. Задайте массив ингредиентов. 
+
+
+   let buuza = 
     {
-        buuza1: 'buuza1', 
-        price: 80, 
+        name: 'позы', 
+        price: 0, 
         expense: 30, 
-        ingridients: ['meat', 'onion', 'dough', 'eggs',],
-        },
+        ingredients: ['meat', 'onion', 'dough', 'eggs'],
+        };
 
-    { 
-        pies: 'pies', 
-        price: 50, 
+   let  pies = 
+   { 
+        name: 'пирожки', 
+        price: 0, 
         expense: 15, 
-        ingridients: ['onion', 'eggs', 'rise','dough','meat'],},
+        ingredients: ['onion', 'eggs', 'rise','dough','meat'],
+    };
 
-    { 
-        salad: 'salad', 
-        price: 40, 
+   let salad = 
+        { 
+        name: 'салат', 
+        price: 0, 
         expense: 10, 
-        ingridients: ['onion', 'cucumber', 'tomato', 'oliveoli', ],},
+        ingredients: ['onion', 'cucumber', 'tomato', 'oliveoli' ],
+    };
 
-
-    {   wafles: 'wafles',
-        price : 50,  
+let wafles = 
+    {   name: 'вафли',
+        price : 0,  
         expense:20,         
-        ingridients:  [  'eggs', 'dough','sugar', ] , }
-    ]
+        ingredients:  [  'eggs', 'dough','sugar', ]  
+    } ;
+    
+// массив из блюд
+
+let dish = [
+            buuza,
+             pies,
+            salad,
+            wafles
+
+                   ];
 
 
+//Отдельно создайте объект хранящий стоимость этих ингредиентов. 
+
+let ingredientsDish = { 
+ meat: 40,
+ onion: 15,
+ dough: 10,
+ eggs: 10,
+ rise: 12,
+ cucumber: 5,
+ tomato: 8,
+ oliveoli: 3,
+ sugar: 6,       };
+
+//Посчитайте стоимость приготовления динамически (с помощью цикла)
+for (let i = 0; i < dish.length; i++) {
+    let sum = 0;
+    for (let j = 0; j <dish[i].ingredients.length; j++) {
+        sum += ingredientsDish[dish[i].ingredients[j]];
+    }
+    dish[i].price = sum;
+};
+
+console.log(`Созданный массив из объектов блюд cо стоимостью: ${JSON.stringify(dish)}`);
+
+
+//Посчитайте с помощью цикла профит для каждого блюда и запишите его в объект блюда. 
+for (let i = 0; i < dish.length; i++) {
+    dish[i].profit = dish[i].priceSale - dish[i].priceCooking;
+};
+
+console.log(`Созданный массив из объектов блюд с профитом: ${JSON.stringify(dish)}`);
