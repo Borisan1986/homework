baa6c25dce2f325a6b704d28b0c4b233f55efbcf
Your Name <you@example.com>
2023-03-06 12:25:18 -0500

    й1

--- homework11jsfunktion/script.js
+++ homework11jsfunktion/script.js
@@ -1,3 +1,5 @@
+//Напишите функцию принимающую два числа и возвращающую меньшее из них.
+
 function min(a, b) {
     if (a < b) {
       return a;
@@ -9,3 +11,41 @@ function min(a, b) {
   alert(result);
   let result1 = min(10,15);
   alert(result1);
+
+
+  //Напишите функцию, которая будет считать профит от продажи блюда из прошлого задания. 
+  //В эту функцию должны передаваться все данные для расчета профита,
+  // результат расчетов присвойте в объект блюда.
+
+  let buuza = 
+    {
+        name: 'позы', 
+        price: 80, 
+        expense: 30, 
+        ingredients: ['meat', 'onion', 'dough', 'eggs'],
+        };
+
+   let  pies = 
+   { 
+        name: 'пирожки', 
+        price: 30, 
+        expense: 15, 
+        ingredients: ['onion', 'eggs', 'rise','dough','meat'],
+    };
+
+   let salad = 
+        { 
+        name: 'салат', 
+        price: 25, 
+        expense: 10, 
+        ingredients: ['onion', 'cucumber', 'tomato', 'oliveoli' ],
+    };
+
+let wafles = 
+    {   name: 'вафли',
+        price : 40,  
+        expense:20,         
+        ingredients:  [  'eggs', 'dough','sugar', ]  
+    } ;
+    
+
