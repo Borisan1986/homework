30d5c753366d6974def02bf33efa61fbe390d672
Your Name <you@example.com>
2023-03-06 13:07:21 -0500

    q1

--- homework11jsfunktion/script.js
+++ homework11jsfunktion/script.js
@@ -49,10 +49,10 @@ let wafles =
     } ;
     
 
-    function get(a, b){
-        return Math.abs(a - b);
-      }
-      
-      console.info(get(10, 20));   // 10
-      console.info(get(-5, 3));    // 8
-      console.info(get(-13, -15)); // 2
+function get(a, b){
+  return Math.abs(a - b);
+}
+
+console.info(get(10, 20));   // 10
+console.info(get(-5, 3));    // 8
+console.info(get(-13, -15)); // 2
