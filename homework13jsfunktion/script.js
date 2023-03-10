a7b5c4c7c6dadf04284298723f382457a4e79f8c
Your Name <you@example.com>
2023-03-10 10:49:54 -0500

    q1

--- homework13jsfunktion/script.js
+++ homework13jsfunktion/script.js
@@ -71,3 +71,21 @@ sugar: 6,       };
   //Ну и желательно чтобы ты мог в функцию передать ингредиенты блюда и стоимость,
   // а она должна посчитать профит)
   
+//Посчитайте стоимость приготовления динамически (с помощью цикла)
+for (let i = 0; i < dish.length; i++) {
+  let sum = 0;
+  for (let j = 0; j <dish[i].ingredients.length; j++) {
+      sum += ingredientsDish[dish[i].ingredients[j]];
+  }
+  dish[i].price = sum;
+  };
+  
+  console.log(`Созданный массив из объектов блюд cо стоимостью: ${JSON.stringify(dish)}`);
+  
+  
+  //Посчитайте с помощью цикла профит для каждого блюда и запишите его в объект блюда. 
+  for (let i = 0; i < dish.length; i++) {
+  dish[i].profit = dish[i].price - dish[i].expense;
+  };
+  
+  console.log(`Созданный массив из объектов блюд с профитом: ${JSON.stringify(dish)}`);
