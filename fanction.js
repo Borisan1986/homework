96e98e48a38ba18e83c2ee6030eb8bfe75c53415
Your Name <you@example.com>
2023-03-24 13:29:10 -0400

    q1

---
+++ fanction.js
@@ -0,0 +1,9 @@
+  // создали функция для посчета профита от продажи блюда
+  function countProfit(price, expense) {
+    return price - expense;
+  }
+
+  function minValue(a, b) {
+    result = (a < b) ? a : b;
+    return result;
+  }
