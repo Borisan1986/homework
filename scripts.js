606029685ef9a6751f987b5792720e7522652e32
Your Name <you@example.com>
2023-03-27 12:21:58 -0400

    q1

--- scripts.js
+++ scripts.js
@@ -1,16 +1,6 @@
 
 import { getValue, mapArray,p } from './module.js';
 
-
-// const superHero = [
-//     { id: 12345, hero: 'batman', power: 'money' },
-//     { id: 54321, hero: 'aquaman', power: 'fishpower' },
-// ];
-
-
-
-
-
  const loginPass = [
     {boris: '1986',
     admin: 'root',
@@ -39,8 +29,6 @@ alert ('оператор не найден!');
 else if (pass !== loginPass.boris||pass !== loginPass.admin||pass!== loginPass.user){
 alert('пароль не верный!');
 };
-// import getValue from './modules/get_value.js';
-// import mapArray from './modules/map.js';
-// import {getValue, mapArray} from './modules/helpers_array.js';
+
 
 
