918b72bea5172186f17d163cea894e9b8187b243
Your Name <you@example.com>
2023-03-30 17:22:47 -0400

    q1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+import { vegetarianDish } from './function.js';
    let buuza = 
     {
         name: 'позы', 
@@ -41,6 +41,8 @@ let dish = [
                    ];
 
 
+
+
 //Отдельно создайте объект хранящий стоимость этих ингредиентов. 
 
 let ingredientsDish = { 
@@ -54,32 +56,15 @@ let ingredientsDish = {
  oliveoli: 3,
  sugar: 6,       };
 
-//Посчитайте стоимость приготовления динамически (с помощью цикла)
-for (let i = 0; i < dish.length; i++) {
-    let sum = 0;
-    for (let j = 0; j <dish[i].ingredients.length; j++) {
-        sum += ingredientsDish[dish[i].ingredients[j]];
-    }
-    dish[i].price = sum;
-};
 
-console.log(`Созданный массив из объектов блюд cо стоимостью: ${JSON.stringify(dish)}`);
-
-
-// //Посчитайте с помощью цикла профит для каждого блюда и запишите его в объект блюда. 
+// //Посчитайте стоимость приготовления динамически (с помощью цикла)
 // for (let i = 0; i < dish.length; i++) {
-//     dish[i].profit = dish[i].priceSale - dish[i].priceCooking;
+//     let sum = 0;
+//     for (let j = 0; j <dish[i].ingredients.length; j++) {
+//         sum += ingredientsDish[dish[i].ingredients[j]];
+//     }
+//     dish[i].price = sum;
 // };
 
-// console.log(`Созданный массив из объектов блюд с профитом: ${JSON.stringify(dish)}`);
+// console.log(`Созданный массив из объектов блюд cо стоимостью: ${JSON.stringify(dish)}`);
 
-
-// //1. В задании к предыдущему уроку в пункте 3 создайте локальные переменные внутри цикла и запишите в них строку с основной информацией по блюду, которую вы бы поместили в печатное меню.
-// //2. Так же в этом задании создайте глобальную переменную и запишите в нее все строки строки из пункта 1 этого задания. В результате вы должны получить меню со списком всех блюд, которое можно было бы распечатать.
-// let allDescriptionMenu = '';
-
-// for (let i = 0; i < dish.length; i++) {
-//     let description = `Название блюда: ${dish[i].name}; Стоимость: ${dish[i].expense}\n`;
-//     allDescriptionMenu += description;
-// };
-// console.log(`Меню:\n${allDescriptionMenu}`);
