cddda1e0d3dfd19c1979acde8a65f84d1913dc93
Your Name <you@example.com>
2023-04-12 17:27:41 -0400

    q1

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let menu;
+
 fetch("/food.json")
+.then((r) => r.json())
+.then((r) => {
+menu = r;
+return fetch("/ingredients.json");
+})
 .then((r) => r.json())
 .then((r) => console.log(r));
 
-// console.log("after fetch");
