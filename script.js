6b86a92dc38585d10872f47c47bdbd3819acd1d0
Your Name <you@example.com>
2023-04-13 21:21:35 -0400

    q1

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 let menu;
 
-fetch("/food.json")
+let ingredients;
+
+fetch("./food.json")
 .then((r) => r.json())
 .then((r) => {
 menu = r;
-return fetch("/ingredients.json");
+return fetch("./ingredients.json");
 })
 .then((r) => r.json())
-.then((r) => console.log(r));
+.then((r) => {
+    ingredients = r;
+
+		//reduce
+		menu.forEach(function (item) {
+			item.expense = item.ingridients.reduce(function (accum, item) {
+				return accum += expenseIngr[item];
+			}, 0);
+		});
+
+		dish.forEach(function (item) {
+			console.log(`блюдо: ${item.name} - себестоимость: ${item.expense}\n
+			ПРОФИТ ${item.price - item.expense}`);
+		});
+	})
+	// .catch((error) => console.log(error));
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
+ 
 
