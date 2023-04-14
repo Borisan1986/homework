c892956f7e455034ad00ecb6d021b657e19d9253
Your Name <you@example.com>
2023-04-14 17:08:37 -0400

    й1

--- script.js
+++ script.js
@@ -3,55 +3,45 @@ let menu;
 let ingredientsD;
 
 fetch("./ingredientsDish.json")
-.then((r) => r.json())
-.then((r) => {
-    ingredientsD = r;
-return fetch("./food.json");
-})
-.then((r) => r.json())
-.then((r) => {
-    menu = r;
+    .then((r) => r.json())
+    .then((r) => {
+        ingredientsD = r;   //ингредиенты 
 
-    menu.forEach(function (item, index, arr) {
+        return fetch("./food.json");
+    })
+    .then((r) => r.json())
+    .then((r) => {
+        menu = r;           //позы, пирожки, салат.
+                   //reduce
+     menu.forEach(function (item, index, arr) {
 
-        item.expense = item.ingredients.reduce(function (accum, item,) {
-            return accum += menu[item];
-    
-        }, 0)
-    });
-    
-    
-    menu.forEach(function (item, index, arr) {
-        console.log(`Блюдо: ${item.name} ; Себестоимость: ${item.price}\n
-        ПРОФИТ ${item.price - item.expense}`);
-
- });
-
-
-
-   
-   
-   	//map
-		let changeDish =menu .map(function (item) {
-			let change = {
-				onlyname: item.name,
-				onlyprice: item.price
-			}
-			return change;
-		});
-		console.log(`${JSON.stringify(changeDish)}`);
-   
-    });
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-
+let sumMenuLength = item.ingredients.reduce(function (sum, item, index, arr) {
+ return sum += ingredientsD[item.nameIngrediets];
+                
+ }, 0)
  
+ });
+                
+                
+ menu.forEach(function (item, index, arr) {
+  console.log(`Блюдо: ${item.name} ; Себестоимость: ${item.price}`);
+                });
+                
+     
+
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
