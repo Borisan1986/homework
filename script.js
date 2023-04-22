1cb71d0c015f1bb278efa323a0a19597f8c7d3a4
Your Name <you@example.com>
2023-04-22 17:06:43 -0400

    й1

--- script.js
+++ script.js
@@ -12,36 +12,31 @@ fetch("./ingredientsDish.json")
     .then((r) => r.json())
     .then((r) => {
         menu = r;           //позы, пирожки, салат.
-        console.log(menu);
+ 
+menu.forEach(function (item, index, arr) {
+    console.log(menu);
+    item.price = item.ingredients.reduce(function (sum, item, index, arr) {
+        return sum += ingredientsD[item.nameIngrediets];
+
+    }, 0)
+});
 
 
-        //reduce
-
-        menu.forEach(function (item, index, arr) {
-
-            item.price  = item.ingredients.reduce(function (sum, item, index, arr) {
-                return sum += ingredientsD[item.nameIngrediets];
-              
-            }, 0)
-          
-        });
-        
-        
-        menu.forEach(function (item, index, arr) {
-            console.log(`Блюдо: ${item.name} ; Себестоимость: ${item.price}`);
+menu.forEach(function (item, index, arr) {
+    console.log(`Блюдо: ${item.name} ; Себестоимость: ${item.price}`);
+});
             
 // 2 задание.map для создания массива с объектами
 
 
-let updatedMeals = menu.map(function (item, index, arr) {
-    let elem = {
-        name: item.name,
-        expense: item.expense,
+// let updatedMeals = menu.map(function (item, index, arr) {
+//     let elem = {
+//         name: item.name,
+//         expense: item.expense,
       
-    }
-    return elem;
-});
+//     }
+//     return elem;
+// });
 
-console.log(updatedMeals)
+// console.log(updatedMeals)
         });
-    }); 
