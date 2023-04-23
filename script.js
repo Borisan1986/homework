dfb51210b2b7b60bb09d05650602d55a0f985eff
Your Name <you@example.com>
2023-04-23 11:37:08 -0400

    q1

--- script.js
+++ script.js
@@ -6,20 +6,22 @@ fetch("./ingredientsDish.json")
     .then((r) => r.json())
     .then((r) => {
         ingredientsD = r;   //ингредиенты 
-        console.log(ingredientsD);
+        //console.log(ingredientsD);
         return fetch("./food.json");
     })
     .then((r) => r.json())
     .then((r) => {
         menu = r;           //позы, пирожки, салат.
-        console.log(menu);
+       
+
 menu.forEach(function (item, index, arr) {
-    console.log(menu);
+   
     item.price = item.ingredients.reduce(function (sum, item, index, arr) {
-        return sum += ingredientsD[item.price];
+     
+        return sum += ingredientsD[item].price;
 
     }, 0)
-    console.log(item);
+   
 });
 
 
@@ -30,14 +32,44 @@ menu.forEach(function (item, index, arr) {
 // 2 задание.map для создания массива с объектами
 
 
-// let updatedMeals = menu.map(function (item, index, arr) {
-//     let elem = {
-//         name: item.name,
-//         expense: item.expense,
+let updatedMeals = menu.map(function (item, index, arr) {
+    let elem = {
+        name: item.name,
+        expense: item.expense,
       
-//     }
-//     return elem;
-// });
+    }
+    return elem;
+});
 
-// console.log(updatedMeals)
+console.log(updatedMeals)
+//3 задание .Определите, есть ли в меню хоть одно вегетарианское блюдо, используя `some`.
+
+let veganDish = menu.some(function (item, index, arr) {
+
+    return item.ingredients.every(function(item,index,arr) {
+    return ingredientsD[item].vegan;
+});
+});
+
+console.log(`Есть ли вегетарианское блюдо ${veganDish}`)  // true
+
+//4 задание.Определите, полностью ли у вас вегетарианское меню с помощью `every`.
+
+let vegetarianDishOnly = menu.every(function (item, index, arr) {
+    return item.ingredients.every(function(item,index,arr) {
+        return ingredientsD[item].vegan;
+    });
+});
+
+console.log(`полностью ли у вас вегетарианское меню ${vegetarianDishOnly}`); // folse
+
+
+//Создайте массив с вегетарианскими блюдами с помощью filter.
+
+
+let massivDish = menu.filter(function (item) {
+	return item.greenPees == 'vegan';
+});
+console.log(`массив с вегетарианскими блюдами ${JSON.stringify(massivDish)}`);
+
         });
